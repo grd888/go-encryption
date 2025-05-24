243b28f358042492ea2bdca2c25f74470cb7923a
Greg Delgado <grd888@gmail.com>
2025-05-25 07:51:46 +0800

    track stream cipher state

--- streamCiphers/main.go
+++ streamCiphers/main.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 func crypt(textCh, keyCh <-chan byte, result chan<- byte) {
 	// Use defer to ensure result channel is closed when function exits
 	defer close(result)
 
 	var text, key byte
 	var textOk, keyOk bool
-
+  var count int
 	for {
 		text, textOk = <-textCh
 		if !textOk {
@@ -21,5 +23,7 @@ func crypt(textCh, keyCh <-chan byte, result chan<- byte) {
 		}
 
 		result <- text ^ key
+		count++
+		fmt.Println("Crypted byte: ", count)
 	}
 }
