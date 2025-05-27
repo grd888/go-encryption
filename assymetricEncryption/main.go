a8c42f4a75177bf51fdefd589b56a4ca8127de88
Greg Delgado <grd888@gmail.com>
2025-05-28 07:08:10 +0800

    assymetric encryptioin

---
+++ assymetricEncryption/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+)
+
+func genKeys() (pubKey *ecdsa.PublicKey, privKey *ecdsa.PrivateKey, err error) {
+	privKey, err = ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	pubKey = &privKey.PublicKey
+	return pubKey, privKey, nil
+}
